9200131b037fe977dd3729ff3c49e4a28aded1c3
rob <robcannonxyz@gmail.com>
2020-05-28 10:48:25 -0700

    init from CC:

---
+++ test/terraform_defaults_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"testing"
+)
+
+func TestTerraformDefaults(t *testing.T) {
+	t.Parallel()
+
+	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/defaults")
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: exampleFolder,
+		Vars: map[string]interface{}{
+			//"public_key_path":  publicKeyPath,
+			//"private_key_path": privateKeyPath,
+		},
+	}
+
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		terraform.Destroy(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "setup", func() {
+		terraform.InitAndApply(t, terraformOptions)
+	})
+}
